012fbc06f5d26e8471404c422d7827b078054e11
Anna <a.rcottrill521@gmail.com>
2024-12-14 15:51:22 -0500

    feat: create basic layouts

---
+++ content/config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from 'astro:content';
+
+const personalCollection = defineCollection({
+    schema: z.object({
+        title: z.string(),
+    })
+});
+
+const fccCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+  })
+});
+
+
+export const collections = {
+  'personal': personalCollection,
+  'fcc': fccCollection
+};
