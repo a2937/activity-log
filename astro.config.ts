4920353e022ef375f212d317e343496ca2b9bab1
Anna <a.rcottrill521@gmail.com>
2024-12-14 16:08:28 -0500

    based?

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,7 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://a2937.github.io',
+  base: "/activity-log",
   trailingSlash: "always",
   integrations: [preact()],
 });
