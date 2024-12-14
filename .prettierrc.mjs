eeabb40d5a024f343f6a4c1fa2cd4b319508a26c
Anna <a.rcottrill521@gmail.com>
2024-12-14 16:00:20 -0500

    set package version

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,7 +1,5 @@
 /** @type {import("prettier").Config} */
 export default {
-  site: 'https://a2937.github.io',
-  base: 'activity-log',
   plugins: ["prettier-plugin-astro"],
   overrides: [
     {
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # A2937's Activity log
+
+Live at https://a2937.github.io/activity-log
